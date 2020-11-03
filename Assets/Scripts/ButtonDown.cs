1e64533b23232ab34e25c9f4a31fc08d89743856
Louise <brycelouise@gmail.com>
2020-11-03 16:20:04 +0000

    Scene 3
    03/11 - Level ideas, scene 3

---
+++ Assets/Scripts/ButtonDown.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDown : MonoBehaviour
+{
+    [SerializeField] private GameObject slidingDoor;
+    private Rigidbody2D rgb;
+
+    private bool playerClose = false;
+
+    private void Start()
+    {
+        rgb = slidingDoor.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            playerClose = true;
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            playerClose = false;
+
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E) && playerClose)
+        {
+            Debug.Log("Player pressed top button");
+
+            rgb.velocity = new Vector3(200, 0, 0);
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/SpinWheel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinWheel : MonoBehaviour
+{
+  
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * -1);
+    }
+}
