d10658876b13a35ab75f9f01cdf6b16e60910182
Louise <brycelouise@gmail.com>
2020-11-03 13:44:58 +0000

    File system fix

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour
+public class SceneSwitcher : MonoBehaviour
 {
-
-    
     // Start is called before the first frame update
     void Start()
     {
