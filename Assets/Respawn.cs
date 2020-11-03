d7487f793e29433fd00c106c17abd3fb872470b8
Michael Saiger <mjs638@york.ac.uk>
2020-11-03 13:09:35 +0000

    Added Respawn Script and rigid2d (static) to switch (error occurred for me)

---
+++ Assets/Respawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject spawn;
+    public GameObject BoxSpawn;
+
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.transform.position = spawn.transform.position;
+        }
+       
+        if (collision.tag == "GravityItem")
+        {
+            collision.transform.position = BoxSpawn.transform.position;
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
