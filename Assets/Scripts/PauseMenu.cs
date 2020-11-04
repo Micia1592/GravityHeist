c1d1a693326844fa6a12e3a0a21704f5173c6532
Louise <brycelouise@gmail.com>
2020-11-04 13:50:43 +0000

    Added a pause menu + prefab

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+
+    private bool isPaused;
+    public Canvas pauseCanvas;
+
+    void Start()
+    {
+        isPaused = false;
+        pauseCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+     
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                pauseCanvas.enabled = true;
+                Time.timeScale = 0;
+                
+
+                //  showPaused();
+            }
+            else if (Time.timeScale == 0)
+            {
+                pauseCanvas.enabled = false;
+                Time.timeScale = 1;
+                // hidePaused();
+            }
+        }
+
+    }
+
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseCanvas.enabled = false;
+    }
+}
+    
