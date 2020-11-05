df8b3f9c1b9361161774c765509d4714379c51ab
Louise <brycelouise@gmail.com>
2020-11-05 12:47:53 +0000

    Gravity Gun now looks at mouse direction. Add sprite to 'gravgun'

--- Assets/Scripts/Gravgun.cs
+++ Assets/Scripts/Gravgun.cs
@@ -8,6 +8,8 @@ public class Gravgun : MonoBehaviour
     public float speed = 10f;       //speed of the projectile
 
     private PlayerControls playerCtrl;  //referencing the playercontrol script
+    private Transform aimTransform;
+    
 
     private void Awake()
     {
@@ -17,20 +19,26 @@ public class Gravgun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Where is the mouse 
+        Vector3 mouseDirection;
+        mouseDirection = Input.mousePosition;
+        mouseDirection.z = 0.0f;
+        mouseDirection = Camera.main.ScreenToWorldPoint(mouseDirection);
+        mouseDirection = mouseDirection - transform.position;
+        this.transform.right = mouseDirection - transform.position; //sprite direction
+       
         //if the fire button is pressed
-        if(Input.GetKeyDown("space"))
+        if (Input.GetKeyUp(KeyCode.Mouse0))
         {
+
+          
+
             //add audiosource here and/or animation
-            if(playerCtrl.facingRight)
-            {
-                Rigidbody2D projectileInstance = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
-                projectileInstance.velocity = new Vector2(speed, 0);
-            }
-            else
-            {
-                Rigidbody2D projectileInstance = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, 180f))) as Rigidbody2D;
-                projectileInstance.velocity = new Vector2(-speed, 0);
-            }
+            //Shoot in that direction
+            
+            Rigidbody2D projectileInstance = Instantiate(projectile, transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as Rigidbody2D;
+            projectileInstance.velocity = new Vector2(mouseDirection.x * speed, mouseDirection.y* speed);
+         
         }
     }
 }
