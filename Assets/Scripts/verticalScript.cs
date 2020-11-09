5a1637565c020dea34947a5edd85f4e5ab7dde3f
Michael Saiger <mjs638@york.ac.uk>
2020-11-09 10:39:29 +0000

    Level design and new sprites

---
+++ Assets/Scripts/verticalScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class verticalScript : MonoBehaviour
+{
+    [SerializeField] private float maxDisplacement;
+
+    private Vector2 startPosition;
+
+    [SerializeField] private float moveSpeed = 1f;
+    private float localSpeed;
+
+    private Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        startPosition = this.transform.position;
+        localSpeed = moveSpeed;
+        rb2D = this.GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(localSpeed, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If were outside our set bounds, reverse direction
+        if (Mathf.Abs(startPosition.y - transform.position.y) > maxDisplacement)
+        {
+
+            if (startPosition.y < transform.position.y)
+            {
+                localSpeed = -moveSpeed;
+            }
+            else
+            {
+                localSpeed = moveSpeed;
+            }
+            rb2D.velocity = new Vector2(localSpeed, 0);
+
+        }
+    }
+}
