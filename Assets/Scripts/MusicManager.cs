85ac8891e6ecfff0f787f8a2a5b58536fc028b7c
Louise <brycelouise@gmail.com>
2020-11-10 14:50:50 +0000

    Added music to menu and folders

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+
+    private Scene scene;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject); //keep music manager running 
+    }
+
+    private void Update()
+    {
+
+        int scene = SceneManager.GetActiveScene().buildIndex;
+
+        if (scene > 3 && scene < 6)
+        {
+            //switch music
+        }
+
+    }
+}
