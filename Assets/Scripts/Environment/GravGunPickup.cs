e1ba113c85c1bd77fe1c68fdf2df803865939eb8
OllieWShouldBeCoding <owithington@hotmail.co.uk>
2020-11-11 16:17:53 +0000

    GravGun Pickup fix
    Gravgun pick up wasnt working as the GravGun was no longer the 0th child object of the player. The gravgun is now a serialised field on the pick up obect

--- Assets/Scripts/Environment/GravGunPickup.cs
+++ Assets/Scripts/Environment/GravGunPickup.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class GravGunPickup : MonoBehaviour
 {
+
+    public GameObject gravGun;
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
         if (collision.gameObject.tag == "Player")
         {
-            Debug.Log("collided");
-            collision.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            //Debug.Log("collided");
+
+            if (gravGun!=null){
+                gravGun.SetActive(true);
+            }
+            else{
+                Debug.Log("GravGunPickup - Unable to find grav gun to activate it");
+            }
             Destroy(gameObject);
         }
     }
