9865a5e2b3e37b99260419c021df1e538a119b99
Michael Saiger <mjs638@york.ac.uk>
2020-11-11 17:20:48 +0000

    Updates to level. Setting level added

--- Assets/Scripts/UI/Continue.cs
+++ Assets/Scripts/UI/Continue.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartGame : MonoBehaviour
+public class Continue : MonoBehaviour
 {
     // Start is called before the first frame update
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene("Setting");
+        SceneManager.LoadScene("Intro_1");
     }
 }
