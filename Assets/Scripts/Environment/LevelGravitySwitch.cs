7808cf675094436b77d65ce55ae95055795c5ace
OllieWShouldBeCoding <owithington@hotmail.co.uk>
2020-11-13 12:17:34 +0000

    Grav Switches change color on enter

--- Assets/Scripts/Environment/LevelGravitySwitch.cs
+++ Assets/Scripts/Environment/LevelGravitySwitch.cs
@@ -10,6 +10,29 @@ public class LevelGravitySwitch : MonoBehaviour
 
     [SerializeField] private GameStateController gameStateController;
 
+    [SerializeField] private SpriteRenderer gravEffectSprite;
+
+    private Material gravMaterial;
+
+    private float currBlueLevel;
+
+    [SerializeField] private float minBlue;
+    [SerializeField] private float maxBlue;
+
+    private bool colorDescending = true;
+
+    [SerializeField] private float colorChangeIncrement;
+
+    private Color gravEffectStartColor;
+
+    private void Start() {
+        gravMaterial = gravEffectSprite.transform.GetComponent<SpriteRenderer>().material;
+        gravEffectStartColor = gravMaterial.color;
+        currBlueLevel = gravEffectStartColor.b;
+
+        //Debug.Log("Current blue level:" + currBlueLevel);
+    }
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.layer == 8){
             playerClose = true;
@@ -22,6 +45,8 @@ public class LevelGravitySwitch : MonoBehaviour
         if (other.gameObject.layer == 8){
             playerClose = false;
 
+            ResetColor();
+
             //Debug.Log("Player left button area");
         }
     }
@@ -33,5 +58,42 @@ public class LevelGravitySwitch : MonoBehaviour
             gameStateController.SetGravityInverted();
 
         }
+
+        //Blink effect
+        if (playerClose){
+            UpdateGravColor();
+            Debug.Log("Current blue level:" + gravMaterial.color.b);
+        }
+    }
+
+    private void UpdateGravColor(){
+
+        //Change color if descending
+        if (colorDescending){
+            Color tempColor = gravMaterial.color;
+            tempColor.b = gravMaterial.color.b-colorChangeIncrement;
+            gravMaterial.color = tempColor;
+
+        }
+        //If ascending
+        else {
+            Color tempColor = gravMaterial.color;
+            tempColor.b = gravMaterial.color.b+colorChangeIncrement;
+            gravMaterial.color = tempColor;
+        }
+
+        //Check if we are below limit
+        if (gravMaterial.color.b < minBlue && colorDescending){
+            colorDescending = false;
+        }
+        //Check if we are above
+        else if (gravMaterial.color.b >= maxBlue && !colorDescending){
+            colorDescending = true;
+        }
+
+    }
+
+    private void ResetColor(){
+        gravMaterial.color = gravEffectStartColor;
     }
 }
